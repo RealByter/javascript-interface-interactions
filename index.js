8c26e3b47153d3ee1fd54ef27c4d88cd440e6cdb
RealByter <justavishay@gmail.com>
2023-05-02 19:19:59 +0300

    feat: implement a dropdown menu

---
+++ index.js
@@ -0,0 +1,20 @@
+const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+
+dropdownMenus.forEach(menu => {
+  for(let i = 0; i < menu.children.length; i++) {
+    menu.children[i].classList.add("invisible")
+    menu.children[i].style.top = `${20 + 20 * i}px`;
+  }
+
+  menu.addEventListener("mouseover", e => {
+    for(let i = 0; i < menu.children.length; i++) {
+      menu.children[i].classList.remove("invisible");
+    }
+  })
+
+  menu.addEventListener("mouseout", e => {
+    for(let i = 0; i < menu.children.length; i++) {
+      menu.children[i].classList.add("invisible")
+    }
+  })
+});
